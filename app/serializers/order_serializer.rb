bc4c64d030244bcc3304a2dc3e69810d96c9189d
Vanessa Toro <vanetoro84@gmail.com>
2018-08-17 16:52:05 +0000

    Done.

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :created_at
-  has_many :products, serializer: SimpleProductSerializer
+  belongs_to :ordered_products
+  has_many :products, through: :ordered_products
 end
